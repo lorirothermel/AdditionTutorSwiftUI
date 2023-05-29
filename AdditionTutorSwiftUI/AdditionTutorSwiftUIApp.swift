95e6d8ff28da4c90717e95bdb376fed99598f036
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-05-28 20:51:21 -0400

    Initial Commit

---
+++ AdditionTutorSwiftUI/AdditionTutorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdditionTutorSwiftUIApp.swift
+//  AdditionTutorSwiftUI
+//
+//  Created by Lori Rothermel on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AdditionTutorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
